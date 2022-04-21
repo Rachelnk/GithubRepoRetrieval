b1165fdd821883d879b08968547bfa9deea13945
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-21 10:28:07 +0300

    Create search function in the users.component.ts.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserserviceService } from '../userservice.service';
+import { Users } from '../users';
+import { Repos } from '../repos';
 
 @Component({
   selector: 'app-users',
@@ -7,8 +9,30 @@ import { UserserviceService } from '../userservice.service';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  user!: Users;
+  repo!: Repos;
 
-  constructor() { }
+  constructor(public myService: UserserviceService, private repoService: UserserviceService) { }
+  search(searchName:any) {
+    this.myService.searchUser(searchName).then(
+      (success: any)=>{
+        this.user = this.myService.foundUser;
+      },
+      (error: any)=>{
+        console.log(error)
+      }
+    );
+      this.repoService.getRepos(searchName).then(
+        (results: any)=>{
+          this.repo =this.repoService.allRepos
+          console.log(this.repo);
+        },
+        (error: any)=>{
+          console.log(error);
+        }
+      );
+
+  }
 
   ngOnInit(): void {
   }
