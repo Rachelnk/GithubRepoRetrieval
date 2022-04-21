08d31cadb4039ac3c03d8e23b2b8881267229b22
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-21 12:48:24 +0300

    Add api key access token.

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,23 @@
+import { HostListener } from '@angular/core';
+import { ElementRef } from '@angular/core';
 import { Directive } from '@angular/core';
+import { threadId } from 'worker_threads';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elemt: ElementRef) { }
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight ('turquoise')
+  }
+  @HostListener('mouseleave') onMouseLeave () {
+    this.highlight("null");
+
+  }
+  private highlight(color:string) {
+    this.elemt.nativeElement.style.backgroundColor = color;
+  }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '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'
 };
 
 /*
