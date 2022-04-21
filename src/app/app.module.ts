369b91fe9dd3db013c8023224b6adfe03d7f134c
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-21 12:08:57 +0300

    Create hightlight directive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RepositoriesComponent } from './repositories/repositories.component';
-import { UserserviceService } from './userservice.service';
+import { HttpClientModule } from '@angular/common/http';
 import { UsersComponent } from './users/users.component';
-import { DatepipePipe } from './datepipe.pipe';
-import { SearchFormComponent } from './search-form/search-form.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UserserviceService } from './userservice.service';
+// import { HighlightDirective } from './highlight.directive';
+import { DatePipe } from '@angular/common';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
+import { Users } from './users';
+import { DatepipePipe } from './datepipe.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
@@ -17,13 +24,17 @@ import { NavbarComponent } from './navbar/navbar.component';
     RepositoriesComponent,
     UsersComponent,
     DatepipePipe,
-    SearchFormComponent,
     NotFoundComponent,
-    NavbarComponent
+    SearchFormComponent,
+    NavbarComponent,
+    HighlightDirective
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [UserserviceService],
   bootstrap: [AppComponent]
