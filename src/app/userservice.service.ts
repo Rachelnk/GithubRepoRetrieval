c2c2b10aaa8f03229cefa04305d53b81c5e0fe39
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-21 09:59:16 +0300

    Create getRepos function .

--- src/app/userservice.service.ts
+++ src/app/userservice.service.ts
@@ -17,6 +17,33 @@ export class UserserviceService {
 
    }
    searchUser (searchName: string){
+    interface Response {
+      url:string,
+      login: string;
+      html_url:string;
+      location:string
+      public_repos:number;
+      followers:number;
+      following:number;
+      avatar_url:string;
+      created_at:Date;
+    }
+    return new Promise<void>((resolve, reject) => {
+      this.http.get<Response>('https://api.github.com/users/'+searchName+'?access_token='+environment.apiKey).toPromise().then(
+        (result) => {
+          this.foundUser = result;
+          console.log(this.foundUser);
+          resolve();
+        },
+        (error) => {
+          console.log(error);
+          reject();
+        }
+      );
+    });
 
+   }
+   getRepos (searchName: any){
+     
    }
 }
