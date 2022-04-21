54676e1881c3c406e6a3be95c38dbcd1428946a0
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-21 09:05:53 +0300

    Create a repository search function.

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserserviceService} from '../userservice.service';
+import { Repos } from '../repos';
 
 @Component({
   selector: 'app-repositories',
@@ -6,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
+  repo: Repos;
 
-  constructor() { }
+
+  constructor(public repoService: UserserviceService) { }
+  searchRepo (searchName) {
+    this.repoService.getReopos(searchName).then(
+      (results)=>{
+        this.repo =this.repoService.allRepos
+        console.log(this.repo);
+      },
+      (error)=>{
+        console.log(error);
+      }
+    );
+
+  }
 
   ngOnInit(): void {
   }
