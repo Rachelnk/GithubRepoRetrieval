60c4ed4699561d1caa2e7bd029b5e81707e3dbcb
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-17 20:57:54 +0300

    Update search form in user's profile page.

--- src/app/interface/user.ts
+++ src/app/interface/user.ts
@@ -1,21 +1,21 @@
 export interface UserInterface {
-  id: number,     
-      name: string,
-      blog: string,
-      repos: string,
-      company: string,
-      url:string,
-      login: string,
-      html_url:string,
-      location:string
-      public_repos:number,
-      followers:number,
-      following:number,
-      avatar_url:string,
-      updated_by: string;
-      created_at: string,
-      followers_url: string,
-      repos_url: string,
-      apiKey?: Boolean,
-      apiUrl?: Boolean,
+   
+  id: number;
+  followers: number;
+  following: number;
+  url: string;
+  name: string;
+  blog: string;
+  repos: string;
+  company: string;
+  login: string;
+  html_url:any;
+  updated_at: string;
+  created_at: string;
+  avatar_url: string;
+  followers_url: string;
+  public_repos: string;
+  repos_url: string;
+  apiKey?: Boolean;
+  apiUrl?: Boolean;
 }
