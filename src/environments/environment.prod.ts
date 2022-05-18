decc255422d09205e98a91415ff04167e180de9f
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-18 15:17:57 +0300

    Generate new access token.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   GITHUB_API_URL : `https://api.github.com/users/`,
-  apiKey: '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'
+  apiKey: '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'
 };
